4108b0026ce8bd350414ddd2f9c7698d33636d97
wiktrek <61013382+wiktrek@users.noreply.github.com>
2022-11-20 22:23:43 +0100

    youtube icon position changed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>wiktrek.xyz</title>
         <meta
           name="keywords"
-          content="wiktrek, wiktrek.xyz, wiktrek website, wiktor, wiktorek, xyz, wiktrek xyz, wiktor, wiktorek"
+          content="wiktrek, wiktrek.xyz, wiktrek website, wiktor, wiktorek, xyz, wiktrek xyz, wiktor, wiktorek, wiktorek website, wiktor website, wiktrek.com, wiktrek.pl, wiktrek.org"
         />
         <meta name="description" content="wiktrek's website" />
         <link rel="icon" href="/favicon.ico" />
