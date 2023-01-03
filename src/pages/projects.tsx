1b371893339bd193512018865c7ac6a37bd599c0
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-01-03 20:56:23 +0100

    svelte

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -17,6 +17,9 @@ const Home: NextPage = () => {
         <div>
           <ul className={styles.links}>
             <li>
+              <a href="https://svelte.wiktrek.xyz">svelte</a>
+            </li>
+            <li>
               <a href="https://info.wiktrek.xyz">info</a>
             </li>
             <li>
