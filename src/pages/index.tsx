e2d9843536ea5726f76bebbf699b8b2f3d51ffe4
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-15 21:23:56 +0100

    modified:   src/pages/index.tsx 	deleted:    src/pages/links.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,64 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faGithub,
+  faInstagram,
+  faTwitch,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
+import { faGlobe, faCode } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Home - wiktrek.xyz</title>
+        <title>wiktrek.xyz</title>
+        <meta name="description" content="wiktrek's links" />
       </Head>
-      <div className="">hi</div>
+      <div className="">
+        <ul className="">
+          <li>
+            <a href="https://www.instagram.com/wiktrekxyz" target="__blank">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </li>
+
+          <li>
+            <a href="https://github.com/wiktrek" target="__blank">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/@wiktrek" target="__blank">
+              <FontAwesomeIcon icon={faYoutube} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitch.tv/wiktrek1232" target="__blank">
+              <FontAwesomeIcon icon={faTwitch} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.wiktrek.xyz" target="__blank">
+              <FontAwesomeIcon icon={faGlobe} />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/wiktrek/wiktrekxyz" target="__blank">
+              <FontAwesomeIcon icon={faCode} />
+            </a>
+          </li>
+          <li>
+            <a href="https://project.wiktrek.xyz/" target="__blank">
+              Projects
+            </a>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
