88df413ffea7bddaebb97d02bd529a653c0d1547
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-16 21:19:27 +0100

    middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,14 +4,14 @@ import type { NextRequest } from 'next/server';
 export function middleware(req: NextRequest, res: NextResponse) {
   if (req.nextUrl.pathname.startsWith('/go')) {
     let a = `https://project.wiktrek.xyz/api${req.nextUrl.pathname}`;
-    return NextResponse.rewrite(new URL(a));
+    return NextResponse.redirect(a);
   }
   if (req.nextUrl.pathname.startsWith('/q')) {
     let a = `https://project.wiktrek.xyz/projects/poll${req.nextUrl.pathname}`;
-    return NextResponse.rewrite(new URL(a));
+    return NextResponse.redirect(a);
   }
   if (req.nextUrl.pathname === '/poll') {
     let a = `https://project.wiktrek.xyz/projects/poll`;
-    return NextResponse.rewrite(new URL(a));
+    return NextResponse.redirect(a);
   }
 }
