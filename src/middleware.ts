2f8d1fe15151ca342be6b2d7c8e18b9a168c8071
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-02-26 20:28:27 +0100

    not working

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-
+import data1 from './data/data.json';
 export function middleware(req: NextRequest, res: NextResponse) {
   if (req.nextUrl.pathname.startsWith('/go')) {
     let a = `https://project.wiktrek.xyz/api${req.nextUrl.pathname}`;
@@ -13,5 +13,18 @@ export function middleware(req: NextRequest, res: NextResponse) {
   if (req.nextUrl.pathname === '/poll') {
     let a = `https://project.wiktrek.xyz/projects/poll`;
     return NextResponse.redirect(a);
+  }
+  const data = data1.data;
+  let a = 0;
+  console.log(req.nextUrl.pop('/'));
+
+  data.map((item: any) => {
+    if (l === item.name) {
+      NextResponse.redirect(item.redirect);
+      a++;
+    }
+  });
+  if (a == 0) return;
+  if (req.nextUrl === `/${data}`) {
   }
 }
