90c095c4065fba00833f7b26811e0ab4972a056b
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-15 23:50:56 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
+      },
+    ];
+  },
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
   },
