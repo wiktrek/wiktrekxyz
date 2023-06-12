a02ab062f378d7e137faacb7931b95c304da35d5
wiktrek <wiktor.janiszewski10@gmail.com>
2023-06-12 22:06:40 +0200

    sitemap.xml + robots.txt

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -23,6 +23,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: 'https://wiktrek.xyz/blog',
       lastModified: new Date(),
     },
+    {
+      url: 'https://project.wiktrek.xyz/projects/url',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://project.wiktrek.xyz/projects/poll',
+      lastModified: new Date(),
+    },
   ];
 
   return sitemap;
