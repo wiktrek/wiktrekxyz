f24ddce172b3515c9eb953b0b3a9f45ac55f8a19
wiktrek <wiktor.janiszewski10@gmail.com>
2023-06-12 17:16:44 +0200

    idk

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,24 +1,7 @@
-import { glob, globSync, globStream, globStreamSync, Glob } from 'glob';
-import { MDXRemote } from 'next-mdx-remote/rsc';
-import { join } from 'node:path';
+'use client';
+import DynamicRoutes from '../articles/DynamicRoutes.mdx';
 export default async function Page({ params }: { params: { id: string } }) {
-  const blogDir = join(process.cwd(), 'app/blog');
-  const mdxfiles = await glob('**/*.mdx', {
-    cwd: blogDir,
-    absolute: false,
-  });
-  console.log(mdxfiles);
-
   const { id } = params;
 
-  return (
-    <>
-      <MDXRemote
-        source={`# Hello World
-
-        This is from Server Components!
-        `}
-      />
-    </>
-  );
+  return <DynamicRoutes />;
 }
