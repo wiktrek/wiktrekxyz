c54c13dbf865a6833a77b2a8338c38944422f846
wiktrek <wiktor.janiszewski10@gmail.com>
2023-06-12 16:33:39 +0200

    blog

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,3 @@
 export default function Page({ params }: { params: { id: string } }) {
   const { id } = params;
-
-  return <h1>wiktrek.xyz/blog/{id} </h1>;
 }
--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'github.com' },
@@ -14,4 +24,4 @@ const nextConfig = {
   swcMinify: true,
 };
 
-module.exports = nextConfig;
+module.exports = withMDX(nextConfig);
