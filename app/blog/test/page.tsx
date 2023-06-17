6ebeb308e3a17f6a9a3bc700286eff16eb914b98
wiktrek <wiktor.janiszewski10@gmail.com>
2023-06-17 22:27:03 +0200

    idk

--- app/blog/test/page.tsx
+++ app/blog/test/page.tsx
@@ -4,3 +4,8 @@ import Content from './content.mdx';
 export default function Page() {
   return <Content />;
 }
+const data = {
+  title: 'test',
+  description: 'test.mdx',
+  date: '17.06.2023',
+};
