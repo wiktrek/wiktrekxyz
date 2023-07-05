cebe3e5f77a40b602950b5b0352bd2de0b1c9a76
wiktrek <wiktor.janiszewski10@gmail.com>
2023-07-05 17:36:46 +0200

    Astro!

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('tailwind-scrollbar')],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
