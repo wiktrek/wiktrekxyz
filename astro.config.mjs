bb2533bf1077e89460e9e4053559f410aeb9b521
wiktrek <wiktor.janiszewski10@gmail.com>
2023-07-06 00:56:55 +0200

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,14 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wiktrek.xyz',
   output: 'server',
-  integrations: [
-    tailwind(),
-    mdx(),
-    sitemap({
-      customPages: [
-        'https://project.wiktrek.xyz',
-        'https://svelte.wiktrek.xyz',
-        'https://term.wiktrek.xyz/',
-        'https://project.wiktrek.xyz/projects/url',
-        'https://project.wiktrek.xyz/projects/poll',
-      ],
-    }),
-    react(),
-  ],
+  integrations: [tailwind(), mdx(), sitemap({
+    customPages: ['https://project.wiktrek.xyz', 'https://svelte.wiktrek.xyz', 'https://term.wiktrek.xyz/', 'https://project.wiktrek.xyz/projects/url', 'https://project.wiktrek.xyz/projects/poll']
+  }), react()],
+  adapter: vercel()
 });
