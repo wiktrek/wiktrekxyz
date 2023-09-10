15bc260928059f6b6e79122036625d4b183dd9fa
wiktrek <wiktor.janiszewski10@gmail.com>
2023-09-10 17:44:56 +0200

    middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { defineMiddleware } from 'astro/middleware';
-import { data } from '../data/data.json';
+import { links } from '../data/data.json';
 export const onRequest = defineMiddleware(async (context, next) => {
   const path = context.url.pathname;
   if (path.startsWith('/go')) {
@@ -16,6 +16,12 @@ export const onRequest = defineMiddleware(async (context, next) => {
   }
   if (path === '/github') {
     return context.redirect('https://github.com/wiktrek');
+  }
+  for (let i = 0; i < links.length; i++) {
+    if (path === '/' + links[i].name) {
+      console.log(links[i].redirect);
+      return context.redirect(links[i].redirect);
+    }
   }
   return next();
 });
