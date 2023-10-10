b87f79e3c64a8f83f9e837752ce24a187a93ac97
wiktrek <wiktor.janiszewski10@gmail.com>
2023-10-10 19:44:43 +0200

    PRETTIER

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 function new_date(date: string): Date {
-  var date_components = date.split('.');
+  var date_components = date.split(".");
   var day = Number(date_components[0]);
   var month = Number(date_components[1]);
   var year = Number(date_components[2]);
@@ -15,11 +15,11 @@ const blogSchema = z.object({
     .transform((val) => new_date(val as string)),
   description: z.string(),
   postSlug: z.string(),
-  img: z.string().default(''),
+  img: z.string().default(""),
   featured: z.boolean().default(false),
 });
 const blogCollection = defineCollection({
-  type: 'content',
+  type: "content",
   schema: blogSchema,
 });
 export type BlogSchema = z.infer<typeof blogSchema>;
