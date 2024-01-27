288280d98008ebf2f360f7df025c016bac4f9242
wiktrek <wiktor.janiszewski10@gmail.com>
2024-01-27 14:02:54 +0100

    feat: text under logo on mobile

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -8,10 +8,24 @@ export interface Props {
   category: string;
   text: string;
 }
+import { useEffect, useState } from "react";
+/* 
 
+make text under the logo. Hover doesn't work on mobile
+
+*/
 import { Button } from "~/components/ui/button";
 export default function Card(props: Props) {
   const { category, icon, text } = props;
+  let [isMobile, setMobile] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.innerWidth <= 800 ? setMobile(true) : setMobile(false);
+    }
+    console.log(isMobile);
+  });
+
   return (
     <div className="flex justify-between skill " id={category}>
       <HoverCard>
@@ -19,12 +33,20 @@ export default function Card(props: Props) {
           asChild
           className="bg-primary-foreground rounded-xl hover:bg-accent animate-out transition-all"
         >
-          <img
-            src={`/icons/${icon}.svg`}
-            width={80}
-            height={80}
-            className="p-4 w-16 md:w-20"
-          />
+          <div>
+            <img
+              src={`/icons/${icon}.svg`}
+              width={80}
+              height={80}
+              className="p-4 w-16 md:w-20 mx-auto"
+            />
+
+            {isMobile ? (
+              <p className="text-sm text-primary p-1">{text}</p>
+            ) : (
+              <></>
+            )}
+          </div>
         </HoverCardTrigger>
         <HoverCardContent className="w-18">
           <div className="">
