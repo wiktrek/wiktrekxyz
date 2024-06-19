fff4a8561107a7baa121d2bcc125b523d641f796
wiktor <wiktor.janiszewski10@gmail.com>
2024-06-19 18:58:12 +0200

    feat: change projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,39 +1,20 @@
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "~/components/ui/hover-card";
+
 export interface Props {
   title: string;
   second_title: string;
   description: string;
   span: string;
   link?: string;
+  image?: string;
 }
 import { Button } from "~/components/ui/button";
 export default function Card(props: Props) {
   return (
-    <HoverCard>
-      <HoverCardTrigger asChild>
-        <Button variant="link">
-          <a href={props.link} target="_blank">
-            {props.title}
-          </a>
-        </Button>
-      </HoverCardTrigger>
-      <HoverCardContent className="w-80">
-        <div className="flex justify-between space-x-4">
-          <div className="space-y-1">
-            <h4 className="text-sm font-semibold">{props.second_title}</h4>
-            <p className="text-sm">{props.description}</p>
-            <div className="flex items-center pt-2">
-              <span className="text-xs text-muted-foreground">
-                {props.span}
-              </span>
-            </div>
-          </div>
-        </div>
-      </HoverCardContent>
-    </HoverCard>
+  <div className="w-72 h-44 bg-popover rounded-md text-xl shadow-md text-foreground text-left p-2" onClick={() => window.open(props.link)}>
+    <a className="text-3xl text-primary">{props.title}</a>
+    { props.image != null ? <img src={`https://cdn.jsdelivr.net/gh/wiktrek/wiktrekxyz/data/images/${props.image}`}></img> : ""}
+    <p className="px-2">{props.description}</p>
+    <span className="pl-4 text-sm">{props.span}</span>
+  </div> 
   );
 }
