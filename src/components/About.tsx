b395af5bab03d92c65faf054d892d8d789f53eb1
wiktor <wiktor.janiszewski10@gmail.com>
2024-08-13 02:32:35 +0200

    feat: animation!!! - react-spring

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,23 +1,33 @@
 import { useToast } from "~/components/ui/use-toast";
+import { useSpring, animated } from "@react-spring/web";
 export default function About() {
   const { toast } = useToast();
+  const springs = useSpring({
+    from: { y: -400 },
+    to: { y: 0, x: 0 },
+  });
+  const springs2 = useSpring({
+    from: { x: -600 },
+    to: { y: 0, x: 0 },
+  });
   return (
     <section id="about" className="pt-96 h-[100vh] text-4xl">
-      <p>
+      <animated.p style={{ ...springs }}>
         Hello, I'm{" "}
         <button
           className="text-primary"
           onClick={() => {
             toast({
               title: "Clicked",
-              
             });
           }}
         >
           wiktrek
         </button>
-      </p>
-      <p className="text-xl">I love programming and building stuff</p>
+      </animated.p>
+      <animated.p className="text-xl" style={{ ...springs2 }}>
+        I love programming and building stuff
+      </animated.p>
     </section>
   );
 }
