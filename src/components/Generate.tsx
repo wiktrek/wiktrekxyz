722e67df24f4019396c2925e1fccd8af1680fa24
wiktor <wiktor.janiszewski10@gmail.com>
2025-03-02 03:54:58 +0100

    feat: just google it

---
+++ src/components/Generate.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useToast } from "~/components/ui/use-toast";
+import { Textarea } from "~/components/ui/textarea";
+export default function GenerateLink() {
+  const { toast } = useToast();
+  const [link, setLink] = useState("");
+  function CopyLink() {
+    console.log("Link copied to clipboard");
+    navigator.clipboard.writeText(link);
+    toast({
+      title: "Link copied to clipboard",
+    });
+  }
+  function format(text: string): string {
+    return `https://wiktrek.xyz/just/"${text.replaceAll(" ", "%20").replaceAll("?", "%3F")}"`;
+  }
+  return (
+    <div className="justify-center items-center flex flex-col m-10">
+      <Textarea
+        placeholder="Enter your question here"
+        onChange={(e) => setLink(format(e.target.value))}
+        className="w-96 p-4 m-2 rounded"
+      />
+      <Textarea
+        value={link}
+        className="w-96 p-4 m-2 rounded h-32"
+        placeholder="https://wiktrek.xyz/just/"
+      />
+      <button onClick={CopyLink}>Copy Link</button>
+    </div>
+  );
+}
