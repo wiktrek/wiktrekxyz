6f3cc723b971ee26e54743e8c5ed720484a90909
wiktrek <wiktor.janiszewski10@gmail.com>
2025-07-31 18:01:15 +0200

    feat: changed font to Inter

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -68,6 +68,23 @@ module.exports = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
+    fontSize: {
+      sm: "0.750rem",
+      base: "1rem",
+      xl: "1.333rem",
+      "2xl": "1.777rem",
+      "3xl": "2.369rem",
+      "4xl": "3.158rem",
+      "5xl": "4.210rem",
+    },
+    fontFamily: {
+      heading: "Inter",
+      body: "Inter",
+    },
+    fontWeight: {
+      normal: "400",
+      bold: "700",
+    },
   },
   variants: {
     scrollbar: ["dark"],
